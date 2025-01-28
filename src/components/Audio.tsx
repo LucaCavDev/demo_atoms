e64130d7f42623cf64a34cb06056f05c1a809813
Luca Cavicchioli <luca.cavicchioli.88@gmail.com>
2025-01-28 14:02:40 +0100

    fix math random

--- src/components/Audio.tsx
+++ src/components/Audio.tsx
@@ -1,18 +1,27 @@
-
 'use client';
 
-export default function Audio() {
+import { useEffect, useState } from "react";
 
+export default function Audio() {
+    const [heights, setHeights] = useState<number[]>([]);
+
+    useEffect(() => {
+        setHeights(Array.from({ length: 30 }, () => Math.floor(Math.random() * (40 - 14 + 1)) + 14));
+    }, []);
 
     return (
         <div className='flex justify-center items-center gap-1'>
-            {Array.from({ length: 30 }).map((_, index) => (
-                <div
-                    key={index}
-                    className="w-1 bg-black animate-wave"
-                    style={{ height: `${Math.floor(Math.random() * (40 - 14 + 1)) + 14}px` }}
-                ></div>
-            ))}
+            {heights.length > 0 ? (
+                heights.map((height, index) => (
+                    <div
+                        key={index}
+                        className="w-1 bg-black animate-wave"
+                        style={{ height: `${height}px` }}
+                    ></div>
+                ))
+            ) : (
+                <p>Loading...</p>
+            )}
         </div>
     );
 }
